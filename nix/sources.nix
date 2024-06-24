6699aa870d8a934d07ba9d9177f60b547db0fa58
Lương Việt Hoàng <tcm4095@gmail.com>
2024-06-24 17:55:15 +0700

    update fetcher or something

--- nix/sources.nix
+++ nix/sources.nix
@@ -1,16 +1,28 @@
 { fetchzip
+, fetchurl
 , lib
 , sources ? ./sources.json
 }:
 let
+    drvName = name: "${lib.strings.sanitizeDerivationName name}-src";
+
     fetch_tarball = name: spec: fetchzip {
-        name = lib.strings.sanitizeDerivationName name + "-src";
+        name = drvName name;
         inherit (spec) url sha256;
     };
+
+    fetch_file = name: spec: fetchurl {
+        name = drvName name;
+        inherit (spec) url sha256;
+    };
+
+    fetch_local = _: spec: { outPath = /. + spec.path; };
 
     fetch = name: spec:
         if !(spec ? type) then abort "no type found in spec ${name}"
         else if spec.type == "tarball" then fetch_tarball name spec
-        else abort "spec ${name} has unknown type ${builtins.toJSON spec.type}"
-    ;
+        else if spec.type == "local" then fetch_local name spec
+        else if spec.type == "file" then fetch_file name spec
+        else if spec.type == "git" then abort "unimplemented"
+        else abort "spec ${name} has unknown type ${builtins.toJSON spec.type}";
 in lib.mapAttrs (name: spec: spec // { inherit (fetch name spec) outPath; }) (builtins.fromJSON (builtins.readFile sources))
