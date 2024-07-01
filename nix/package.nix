865ba8b4c7f6975d652a26c2b39b4884bb8a2c7a
Lương Việt Hoàng <tcm4095@gmail.com>
2024-07-01 16:02:53 +0700

    add hashtag to colors

--- nix/package.nix
+++ nix/package.nix
@@ -14,29 +14,29 @@
 , runtimepath ? []
 # base16/base24 colorscheme
 , colorscheme ? {
-    base00 = "191724";
-    base01 = "1f1d2e";
-    base02 = "26233a";
-    base03 = "6e6a86";
-    base04 = "908caa";
-    base05 = "e0def4";
-    base06 = "e0def4";
-    base07 = "524f67";
-    base08 = "eb6f92";
-    base09 = "f6c177";
-    base0A = "ebbcba";
-    base0B = "31748f";
-    base0C = "9ccfd8";
-    base0D = "c4a7e7";
-    base0E = "f6c177";
-    base0F = "524f67";
+    base00 = "#191724";
+    base01 = "#1f1d2e";
+    base02 = "#26233a";
+    base03 = "#6e6a86";
+    base04 = "#908caa";
+    base05 = "#e0def4";
+    base06 = "#e0def4";
+    base07 = "#524f67";
+    base08 = "#eb6f92";
+    base09 = "#f6c177";
+    base0A = "#ebbcba";
+    base0B = "#31748f";
+    base0C = "#9ccfd8";
+    base0D = "#c4a7e7";
+    base0E = "#f6c177";
+    base0F = "#524f67";
   }
 }:
 let
     neovimConfig = (neovimUtils.makeNeovimConfig {
         withPython3 = false;
         withRuby = false;
-
+#
         plugins = (with vimPlugins; [
             nvim-treesitter.withAllGrammars
         ]) ++ lib.pipe (callPackage ./sources.nix {}) [
@@ -50,7 +50,7 @@ let
             makeLuaList = list: "{ ${lib.concatStringsSep "," list} }";
         in ''
             vim.opt.runtimepath:prepend ${makeLuaList (map (str: "'${str}'") runtimepath)}
-            require('base16-colorscheme').setup ${makeLuaList (lib.mapAttrsToList (k: v: "${k}='#${v}'") colorscheme)}
+            require('base16-colorscheme').setup ${makeLuaList (lib.mapAttrsToList (k: v: "${k}='${v}'") colorscheme)}
             require "init"
         '';
     };
