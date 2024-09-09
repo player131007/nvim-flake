762c67d059983236966e6b68526e6de0ebe9d158
Lương Việt Hoàng <tcm4095@gmail.com>
2024-09-09 11:08:44 +0700

    throw instead of abort

--- nix/sources.nix
+++ nix/sources.nix
@@ -19,10 +19,10 @@ let
     fetch_local = _: spec: { outPath = /. + spec.path; };
 
     fetch = name: spec:
-        if !(spec ? type) then abort "no type found in spec ${name}"
+        if !(spec ? type) then throw "no type found in spec ${name}"
         else if spec.type == "tarball" then fetch_tarball name spec
         else if spec.type == "local" then fetch_local name spec
         else if spec.type == "file" then fetch_file name spec
-        else if spec.type == "git" then abort "unimplemented"
-        else abort "spec ${name} has unknown type ${builtins.toJSON spec.type}";
+        else if spec.type == "git" then throw "unimplemented"
+        else throw "spec ${name} has unknown type ${builtins.toJSON spec.type}";
 in lib.mapAttrs (name: spec: spec // { inherit (fetch name spec) outPath; }) (builtins.fromJSON (builtins.readFile sources))
