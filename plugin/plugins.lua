fa5e87521703122a67a358f8098cb67860a333b5
Lương Việt Hoàng <tcm4095@gmail.com>
2025-09-01 20:52:47 +0700

    mor clues

--- plugin/plugins.lua
+++ plugin/plugins.lua
@@ -3,6 +3,24 @@ require('guess-indent').setup {}
 
 -- mini.clue
 local miniclue = require 'mini.clue'
+local function insert_cursor_object_clues()
+  local describe = function(prefix)
+    local make_clue = function(key, desc) return { mode = 'c', keys = prefix .. key, desc = desc } end
+    return {
+      make_clue("<C-f>", "Filename under cursor"),
+      make_clue("<C-p>", "Filename under cursor, expanded with 'path'"),
+      make_clue("<C-w>", "Word under cursor"),
+      make_clue("<C-a>", "WORD under cursor"),
+      make_clue("<C-l>", "Line under cursor"),
+    }
+  end
+
+  return {
+    describe("<C-r>"),
+    describe("<C-r><C-r>"),
+    describe("<C-r><C-o>"),
+  }
+end
 miniclue.setup {
   triggers = {
     -- Leader triggers
@@ -49,6 +67,7 @@ miniclue.setup {
     miniclue.gen_clues.registers(),
     miniclue.gen_clues.windows(),
     miniclue.gen_clues.z(),
+    insert_cursor_object_clues(),
   },
 
   window = {
