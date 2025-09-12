a3f4a78f9bf707f5d573eb5e979f395ac1f5e296
Lương Việt Hoàng <tcm4095@gmail.com>
2025-09-12 13:11:46 +0700

    mini.statusline

--- plugin/plugins.lua
+++ plugin/plugins.lua
@@ -4,6 +4,31 @@ require('guess-indent').setup {}
 -- mini.icons
 require('mini.icons').setup {}
 
+-- mini.statusline
+require('mini.statusline').setup {
+  content = {
+    active = function()
+      local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 100 })
+      local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+      local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+      local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+      local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+      local location      = MiniStatusline.section_location({ trunc_width = 75 })
+
+      return MiniStatusline.combine_groups({
+        { hl = mode_hl,                  strings = { mode } },
+        { hl = 'MiniStatuslineDevinfo',  strings = { diagnostics, lsp } },
+        '%<', -- Mark general truncate point
+        { hl = 'MiniStatuslineFilename', strings = { filename } },
+        '%=', -- End left alignment
+        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+        { hl = mode_hl,                  strings = { location } },
+      })
+    end,
+    inactive = nil,
+  },
+}
+
 -- mini.clue
 local miniclue = require 'mini.clue'
 local function insert_cursor_object_clues()
