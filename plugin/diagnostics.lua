fbc7bb49d14516ee8fd521dbada5ad062bf0b2cb
Lương Việt Hoàng <tcm4095@gmail.com>
2025-09-21 19:28:26 +0700

    some diagnostic options

---
+++ plugin/diagnostics.lua
@@ -0,0 +1,14 @@
+vim.diagnostic.config({
+  virtual_lines = {
+    current_line = true,
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "",
+    },
+  },
+  severity_sort = true,
+})
