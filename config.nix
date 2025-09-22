07a425301ef334a32c354f699ec5c6a285882eac
Lương Việt Hoàng <tcm4095@gmail.com>
2025-09-23 00:10:35 +0700

    remove `extraBinPath` and configure paths manually

--- config.nix
+++ config.nix
@@ -8,13 +8,6 @@
   appName = "nvim";
   desktopEntry = false;
 
-  extraBinPath = [
-    pkgs.nixfmt
-    pkgs.stylua
-
-    pkgs.emmylua-ls
-  ];
-
   initLua = # lua
     ''
       vim.loader.enable() -- enable this asap
@@ -34,7 +27,20 @@
       impure = "~/nvim-flake";
     };
 
-    start = inputs.mnw.lib.npinsToPlugins pkgs ./start.json;
+    start =
+      inputs.mnw.lib.npinsToPlugins pkgs ./start.json
+      ++ lib.singleton (
+        pkgs.writeTextDir "plugin/+paths.lua" ''
+          vim.lsp.config("emmylua_ls", {
+            cmd = { "${lib.getExe pkgs.emmylua-ls}" },
+          })
+
+          require("conform").formatters = {
+            stylua = { command = "${lib.getExe pkgs.stylua}" },
+            nixfmt = { command = "${lib.getExe pkgs.nixfmt}" },
+          }
+        ''
+      );
 
     opt = builtins.attrValues pkgs.vimPlugins.nvim-treesitter.grammarPlugins;
   };
